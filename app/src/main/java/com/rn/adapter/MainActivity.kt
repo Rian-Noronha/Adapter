5111b1a48d46a0c0f353cac4a0044c5c03845eb3
Rian-Noronha <riannoronha011@gmail.com>
2023-10-31 12:30:01 -0300

    Mostrando views na tela.

--- app/src/main/java/com/rn/adapter/MainActivity.kt
+++ app/src/main/java/com/rn/adapter/MainActivity.kt
@@ -2,10 +2,22 @@ package com.rn.adapter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    //0=VW, 1=GM, 2=FIAT, 3=FORD
+    private val vehicles = mutableListOf(
+        Vehicle("Onix", 2018, 1, true, true),
+        Vehicle("Uno", 2007, 2, true, false),
+        Vehicle("Del Rey", 1988, 3, false, true),
+        Vehicle("Gol", 2014, 0, true, true)
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val listView = ListView(this)
+        setContentView(listView)
+        val adapter = VehicleAdapter(this, vehicles)
+        listView.adapter = adapter
     }
 }
--- app/src/main/java/com/rn/adapter/VehicleAdapter.kt
+++ app/src/main/java/com/rn/adapter/VehicleAdapter.kt
@@ -1,32 +1,66 @@
 package com.rn.adapter
 
+import android.content.ClipData.Item
 import android.content.Context
 import android.content.res.TypedArray
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.StringRes
+import com.rn.adapter.databinding.ItemVehicleBinding
+
 
 class VehicleAdapter(
     private val ctx:Context,
-    private val vehicle:List<Vehicle>
+    private val vehicles:List<Vehicle>
 ):BaseAdapter(){
     private val logos:TypedArray by lazy {
         ctx.resources.obtainTypedArray(R.array.logos)
     }
 
-    override fun getCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getCount(): Int = vehicles.size
 
-    override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
-    }
+    override fun getItem(position: Int) = vehicles[position]
 
-    override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
-    }
+    override fun getItemId(position: Int) = position.toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val binding: ItemVehicleBinding
+        val vehicle = vehicles[position]
+
+        if(convertView == null){
+            binding = ItemVehicleBinding.inflate(LayoutInflater.from(ctx), parent, false)
+        }else{
+            binding = ItemVehicleBinding.bind(convertView)
+        }
+
+        val holder = ViewHolder(binding)
+
+        holder.imgLogo.setImageDrawable(logos.getDrawable(vehicle.manufacturer))
+        holder.txtModel.text = vehicle.model
+        holder.txtYear.text = vehicle.year.toString()
+        holder.txtFuel.text = ctx.getString(getFuel(vehicle))
+
+        return binding.root
     }
+
+    companion object{
+        data class ViewHolder(val binding: ItemVehicleBinding){
+            val imgLogo: ImageView = binding.imgLogo
+            val txtModel: TextView = binding.txtModel
+            val txtYear: TextView = binding.txtYear
+            val txtFuel: TextView = binding.txtFuel
+        }
+    }
+
+    @StringRes
+    private fun getFuel(vehicle: Vehicle): Int =
+        if(vehicle.gasoline && vehicle.ethanol) R.string.fuel_flex
+        else if (vehicle.gasoline) R.string.fuel_gasoline
+        else if (vehicle.ethanol) R.string.fuel_ethanol
+        else R.string.fuel_invalid
+
 }
